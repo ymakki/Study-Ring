c81e0855c80d3985ce97fb9a92d54934f78d1b14
ymakki <147698184+ymakki@users.noreply.github.com>
2024-02-03 06:44:12 +0000

    [Add]model definition

--- app/models/study.rb
+++ app/models/study.rb
@@ -5,7 +5,13 @@ class Study < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_one_attached :image
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def get_image
+    (image.attached?) ? image : 'default_image.jpg'
+  end
 
 end
