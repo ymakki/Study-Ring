99595535b1af46810068baefc4462e70db117d12
ymakki <147698184+ymakki@users.noreply.github.com>
2024-02-05 07:03:37 +0000

    [Add]Membership_function

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  before_action :customer_state, only: [:create]
+  before_action :user_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,15 +18,36 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  # ゲストログイン機能
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: "guestuserでログインしました。"
+  end
+
   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
+  # 退会機能
+  def user_state
+    user = User.find_by(email: params[:user][:email])
+    return if user.nil?
+    return unless user.valid_password?(params[:user][:password])
+    if user.is_active
+      sign_in(user)
+      redirect_to root_path
+    else
+      flash[:notice] = "既に退会済みのアカウントです。新規会員登録が必要になります。"
+      redirect_to new_user_registration_path
+    end
+  end
+
+  # ログイン後の遷移先
   def after_sign_in_path_for(resource)
     root_path
   end
 
   def after_sign_out_path_for(resource)
-    new_customer_session_path
+    new_user_session_path
   end
 
 end
