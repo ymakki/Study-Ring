fb6982a4b55ca6a1a5ffae196bb860d98aae176e
ymakki <147698184+ymakki@users.noreply.github.com>
2024-02-25 18:18:55 +0000

    非同期

--- app/controllers/public/review_favorites_controller.rb
+++ app/controllers/public/review_favorites_controller.rb
@@ -1,17 +1,15 @@
 class Public::ReviewFavoritesController < ApplicationController
 
   def create
-    study_review = StudyReview.find(params[:study_review_id])
-    favorite = current_user.review_favorites.new(study_review_id: study_review.id)
+    @review = StudyReview.find(params[:study_review_id])
+    favorite = current_user.review_favorites.new(study_review_id: @review.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    study_review = StudyReview.find(params[:study_review_id])
-    favorite = current_user.review_favorites.find_by(study_review_id: study_review.id)
+    @review = StudyReview.find(params[:study_review_id])
+    favorite = current_user.review_favorites.find_by(study_review_id: @review.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end
