1c471555c6f7f0e6ad400f7c0fe7e64bc95c252b
ymakki <147698184+ymakki@users.noreply.github.com>
2024-02-27 13:02:44 +0000

    [Fix] ゲスト退会

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,7 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
-  before_action :ensure_guest_user, only: [:edit]
+  before_action :ensure_guest_user, only: [:edit, :withdrawal]
 
   def show
     @user = User.find(params[:id])
@@ -80,7 +80,7 @@ class Public::UsersController < ApplicationController
   def ensure_guest_user
     user = User.find(params[:id])
     if user.guest_user?
-      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+      redirect_to user_path(current_user) , notice: "ゲストユーザーに許可していない操作です。"
     end
   end
 
