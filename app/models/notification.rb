8c63835cd9da6e40592685f0874e50f58686eb0d
ymakki <147698184+ymakki@users.noreply.github.com>
2024-02-29 07:58:48 +0000

    Fix

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -18,7 +18,7 @@ class Notification < ApplicationRecord
       "<i class='fa-regular fa-comment'></i>　#{notifiable.user.name}さんがあなたの勉強記録にコメントしました".html_safe
     when "StudyReview"
 
-      "<i class='fa-regular fa-thumbs-up'></i>　#{notifiable&.user&.name}さんがあなたの勉強記録にいいね！と言っています".html_safe
+       "<i class='fa-regular fa-thumbs-up'></i>　#{notifiable&.user&.name}さんがあなたの勉強記録にいいね！と言っています".html_safe if notifiable.present?
 
     when "ReviewFavorite"
       "<i class='fa-regular fa-thumbs-up'></i>　#{notifiable.user.name}さんがあなたのレビューにいいね！と言っています".html_safe
